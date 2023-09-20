a0b3e22eb8b46fa91ec5febc5b92bb97b51ebfd8
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2023-09-20 13:09:33 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,14 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    extend: { 
+      'primary-blue': '#106EB0',
+      'primary-orange': '#EC7000',
+      'primary-gray': '#33303E',
+      'second-gray': '#4e4b59',
+      'gray-phone': '#f4f4f4',
+      'txt-gray': '#7a7786',
+      'opacity-gray': 'rgba(100, 80, 57, 0.1)'
     },
   },
   plugins: [],
