618cfcac465c923a009ff05b163ce717da871c0c
Efraim Cancio <efraimfonseca@gmail.com>
2023-09-20 22:29:09 -0300

    Implementing the following header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,57 +1,43 @@
 import Image from "next/image";
-import Arrow from "../assets/";
+import Arrow from "../assets/arrow-down.svg";
 import Logo from "../assets/logo.svg";
 
-
 export function Header() {
-    return (
-        <header className="">
-            <div>
-                <div>
-                   <Image 
-                    src={Logo}
-                    alt="Logo"
-                   />
-                   <ul>
-                    <li>
-                        <button>
-                            <span>Para você</span>
-                            <Image
-                             src={Arrow}
-                             alt="icone de seta"
-                            />
-                        </button>
-                    </li>
-                    <li>
-                        <button>
-                            <span></span>
-                             <Image
-                             src={Arrow}
-                             alt="icone de seta"
-                            />
-                        </button>
-                    </li>
-                    <li>
-                        <button>
-                            <span />
-                             <Image
-                             src={Arrow}
-                             alt="icone de seta"
-                            />
-                        </button>
-                    </li>
-                    <li>
-                        <button>
-                            <span />
-                             <Image
-                             src={Arrow}
-                             alt="icone de seta"
-                            />
-                        </button>
-                    </li>
-                   </ul>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header className="w-full h-20 bg-primary-orange flex items-center">
+      <div className="w-full max-w-[1246px] px-[15px] mx-auto">
+        <div>
+          <div className="flex items-center gap-14">
+            <Image src={Logo} alt="Logo" />
+            <ul>
+              <li>
+                <button className="flex items-center gap-3">
+                  <span className="text-white font-bold ">Para você</span>
+                  <Image src={Arrow} alt="icone de seta" />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <span></span>
+                  <Image src={Arrow} alt="icone de seta" />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <span />
+                  <Image src={Arrow} alt="icone de seta" />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <span />
+                  <Image src={Arrow} alt="icone de seta" />
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,14 +10,16 @@ const config: Config = {
     backgroundImage: {
       'img_bg_hero': "url('../assets/bg-hero.jpg')",
     },
-    extend: { 
-      'primary-blue': '#106EB0',
-      'primary-orange': '#EC7000',
-      'primary-gray': '#33303E',
-      'second-gray': '#4e4b59',
-      'gray-phone': '#f4f4f4',
-      'txt-gray': '#7a7786',
-      'opacity-gray': 'rgba(100, 80, 57, 0.1)'
+    extend: {
+      colors: {
+        'primary-blue': '#106EB0',
+        'primary-orange': '#EC7000',
+        'primary-gray': '#33303E',
+        'second-gray': '#4e4b59',
+        'gray-phone': '#f4f4f4',
+        'txt-gray': '#7a7786',
+        'opacity-gray': 'rgba(100, 80, 57, 0.1)'
+      }
     },
   },
   plugins: [],
