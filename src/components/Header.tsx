235fd56dcb2822f07f052809bb0dceece57b4918
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2023-09-25 22:05:26 -0300

    Finalizando a section hero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import IconUser from '../assets/icon-user.svg';
 import Logo from '../assets/logo.svg';
+import { Container } from './Container';
 import { ItemMenu } from './ItemMenu';
 import { Search } from './Search';
 
@@ -9,7 +10,7 @@ export function Header() {
   return (
     <header className="relative w-full h-20 bg-primary-orange flex items-center">
       <div className='absolute top-0 right-0 bg-primary-blue w-[19%] h-full z-0'></div>
-      <div className="flex items-center justify-between w-full max-w-[1246px] px-[15px] mx-auto">
+      <Container>
         <div className="flex flex-1 items-center justify-between">
           <div className="flex items-center gap-14">
             <Image src={Logo} alt="Logo" />
@@ -40,7 +41,7 @@ export function Header() {
           <Image src={IconUser} alt="Icone Usuário" />
           <span className='text-white font-semibold'>Acessar conta</span>
         </button>
-      </div>
+      </Container>
     </header>
   );
 }
--- src/components/SectionHero.tsx
+++ src/components/SectionHero.tsx
@@ -1,10 +1,32 @@
-import { Container } from "./Container";
+import ArrowDown from '@/assets/arrow-explorer.svg';
+import ImgAppleStore from '@/assets/btn-apple-store.svg';
+import ImgGooglePlay from '@/assets/btn-google-play.svg';
+import WomanImage from '@/assets/woman.png';
+import Image from 'next/image';
+import { Container } from './Container';
 
 export function SectionHero () {
     return  (
         <section className="w-full h-[704px] bg-img_bg_hero bg-no-repeat bg-center bg-cover">
             <Container>
-                
+                <div className="flex-1 max-w-[497px]">
+                    <h1 className="text-white text-7xl font-bold mb-4">Tenha seu banco na palma de sua mão. </h1>
+                    <p className="text-white text-xl max-w-[408px] mb-8">Todas as operações que vc precisa em um só lugar. Simples, completo e feito para você.</p>
+                    <div className="flex gap-4 mb-24">
+                        <button>
+                            <Image src={ImgAppleStore} alt="Imagem da Apple"/>
+                        </button>
+                        <button>
+                            <Image src={ImgGooglePlay} alt="Imagem da Google"/>
+                        </button>
+                    </div>
+                    <button className='flex items-center gap-3'>
+                        <Image src={ArrowDown} alt="icone seta"/>
+                        <span className='text-white text-sm font-bold'>Continue explorando
+                        </span>
+                    </button>
+                </div>
+                <Image src={WomanImage} alt="Foto da modelo"  className='mr-[-41px]'/>
             </Container>
         </section>
     )
